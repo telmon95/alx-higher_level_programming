585b62cd9dffa4ba02c1affd561d39773b27ea6c
telmon95 <telmonm@gmail.com>
2024-03-12 18:12:22 -0700

    corrections()2nd

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -8,7 +8,7 @@ class Rectangle {
 	}
 
 print () {
-	for (let i = 0; i < this.height; i++ {
+	for (let i = 0; i < this.height; i++) {
 		let s = '';
 		for (let j = 0; j < this.width; j++) {
 			s += 'X';
--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 class Rectangle {
-	constructor (w, h) {
+	constructor(w, h) {
 		if ((w > 0) && (h > 0)) {
 			this.width = w;
 			this.height = h;
 		}
 	}
 
-	print () {
+	print() {
 		for (let i = 0; i < this.height; i++) {
 			let s = '';
 			for (let j = 0; j < this.width; j++) {
@@ -17,13 +17,13 @@ class Rectangle {
 		}
 	}
 
-	rotate {
+	rotate() {
 		const aux = this.width;
 		this width = this.height;
 		this.height = aux;
 	}
 
-	double () {
+	double() {
 		this.width *= 2;
 		this.height *= 2;
 	}
