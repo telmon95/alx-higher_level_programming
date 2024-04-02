23f7ea7f72471f3f3715ef422b4ec91bc36d53b7
telmon95 <telmonm@gmail.com>
2024-04-01 20:20:54 -0700

    another commit 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
-
-# Check if a URL is provided as an argument
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Send a GET request to the URL and display the body of the response
-curl -s -o /tmp/body_response.txt -w "%{http_code}" "$1" | {
+# Sends a GET request to a URL and displays the body of the response if status code is 200
+curl -s -o /dev/null -w "%{http_code}" "$1" | {
     read -r status_code
-    if [ "$status_code" = "200" ]; then
-        cat /tmp/body_response.txt
+    if [ "$status_code" -eq 200 ]; then
+        curl -s "$1"
     fi
-    rm /tmp/body_response.txt
 }
 
